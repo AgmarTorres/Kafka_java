2eda6ec220f6937e0ce77c0e7c60ea1d45046dec
Agmar Torres <agmartorres@gmail.com>
2020-05-05 09:30:48 -0300

    apllications in Kafka

---
+++ sendtokafka/src/main/java/sendtokafka/RsvpsWebSocketHandler.java
@@ -0,0 +1,31 @@
+package sendtokafka;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+
+@Component
+class RsvpsWebSocketHandler extends AbstractWebSocketHandler {
+
+    private static final Logger logger = 
+            Logger.getLogger(RsvpsWebSocketHandler.class.getName());
+        
+    private final RsvpsKafkaProducer rsvpsKafkaProducer;
+
+    public RsvpsWebSocketHandler(RsvpsKafkaProducer rsvpsKafkaProducer) {    
+        this.rsvpsKafkaProducer = rsvpsKafkaProducer;
+    }
+
+    //Aqui a mensagem é capturada 
+    @Override   
+    public void handleMessage(WebSocketSession session,
+            WebSocketMessage<?> message) {
+        logger.log(Level.INFO, "New RSVP:\n {0}", message.getPayload());
+        
+        //Envia as informações para o kafka
+        rsvpsKafkaProducer.sendRsvpMessage(message);        
+    }
+}
